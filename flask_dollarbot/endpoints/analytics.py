1d0f9875b85860b6db267207a546c7c3af5cc672
Prathamesh Thakur <pratham.thakur2902@gmail.com>
2024-11-23 15:46:11 -0500

    Added checks for analytics file.

--- flask_dollarbot/endpoints/analytics.py
+++ flask_dollarbot/endpoints/analytics.py
@@ -1,7 +1,12 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, jsonify
 import endpoints.helper as helper
 
 analytics_bp = Blueprint('analytics', __name__)
+
+def validate_analytics_request(user_id):
+    if not user_id:
+        return False
+    return True
 
 def build_dictionary(expense_record):
     expense_details = expense_record.split(',')
@@ -17,11 +22,18 @@ def build_dictionary(expense_record):
 
 @analytics_bp.route('/<user_id>', methods=['GET'])
 def find_data(user_id = None):
+    if not validate_analytics_request(user_id):
+        return jsonify({'error': 'Bad Request'}), 400
+    
     helper.read_json()
     chat_id = user_id
 
     data = helper.getUserHistory(chat_id)
 
-    result = [build_dictionary(expense_record) for expense_record in data]
-    print("Result:", result)
-    return jsonify(result), 200
+    if data is None:
+        return jsonify({}, 200)
+
+    else:
+        result = [build_dictionary(expense_record) for expense_record in data]
+        print("Result:", result)
+        return jsonify(result), 200
