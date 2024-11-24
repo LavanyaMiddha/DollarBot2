89a7a68338108b4266c3f54f1e4ba0403d7ee3d5
Lavanya Middha <lavanyamiddha@gmail.com>
2024-11-24 14:02:08 -0500

    adding nav bar

--- frontend/src/pages/Alerts.tsx
+++ frontend/src/pages/Alerts.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Container, Flex, Text, Link } from '@chakra-ui/react';
 import axios from "axios";
 
 type NotificationsData = {
@@ -35,6 +36,66 @@ const Alerts = () => {
 
   return (
     <div>
+      <Container>
+        <Flex justifyContent="flex-start" color="black">
+          <Text
+            color="teal"
+            textStyle="3xl"
+            fontWeight="bold"
+            margin="12px 90px 0 0"
+          >
+            Dollar Bot
+          </Text>
+          <Link
+            color="black"
+            textStyle="lg"
+            href="/"
+            fontWeight="medium"
+            margin="18px 35px 0 0"
+            onClick={() => {
+              localStorage.clear();
+            }}
+          >
+            Log Out
+          </Link>
+          <Link
+            color="black"
+            textStyle="lg"
+            fontWeight="medium"
+            margin="18px 35px 0 0"
+            href="/Home"
+          >
+            Home
+          </Link>
+          <Link
+            color="black"
+            textStyle="lg"
+            fontWeight="medium"
+            margin="18px 35px 0 0"
+            href="/BudgetGoals"
+          >
+            Budget Goals
+          </Link>
+          <Link
+            color="black"
+            textStyle="lg"
+            fontWeight="medium"
+            margin="18px 35px 0 0"
+            href="https://github.com/SoftwareEngNoobs/DollarBot"
+          >
+            About
+          </Link>
+          <Link
+            color="black"
+            textStyle="lg"
+            fontWeight="medium"
+            margin="18px 35px 0 0"
+            href="https://github.com/SoftwareEngNoobs/DollarBot"
+          >
+            Help
+          </Link>
+        </Flex>
+      </Container>
       <h2 style={{fontSize: "30px", fontWeight:"bold", marginBottom:"20px"}}>Notifications</h2>
       <p style={{fontSize: "20px", fontWeight:"bold", marginBottom:"20px"}}> Displaying Statistics for current Month and Year</p>
       <table style={{ background: "White", border: "1px solid black", width: "100%", fontSize: "20px", fontWeight:"bold", borderBlockColor:"black", borderBlockStyle:"dotted", padding: "10px", alignContent:"center"}}>
